c708bc8222782d8f9da77aa54a02d1a52f8b9566
Joe <71609532+SantanJoe@users.noreply.github.com>
2024-02-02 15:20:15 +0000

    change request data handling

--- detection/api.py
+++ detection/api.py
@@ -5,12 +5,19 @@ app = Flask(__name__)
 
 @app.route('/position', methods=['POST'])
 def solve():
-    data = request.json
+    debug = request.json.get("debug", False)
+    background_image = request.json.get("background_image", "")
 
-    if "background_image" not in data or data["background_image"] == "":
+
+    # Check if the background_image is in the request
+    if request.json.get("background_image", "") == "":
         return {"message": "background_image is required"}, 400, {'Content-Type': 'application/json'}
     
-    position = SlidePosition(data["background_image"], True)
+    # Get the slide position from the slide class
+    position = SlidePosition(background_image, debug)
+
+    if not hasattr(position, "slide_position"):
+        return {"message": "Error: Could not load the images. Check the image url."}, 400, {'Content-Type': 'application/json'}
 
     return {"position": position.slide_position}, 200, {'Content-Type': 'application/json'}
 
